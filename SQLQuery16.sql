aeef4bcc8f80faedd9c7bed8cb271c448486c309
mbanne001 <mbanne001@gmail.com>
2026-01-15 06:29:37 -0600

    Update SQLQuery16.sql

--- SQLQuery16.sql
+++ SQLQuery16.sql
@@ -1,3 +1,10 @@
+--Q1. Find employees whose salary is exactly 50,000.
+
+--Q2. List products with price greater than 200.
+
+--Q3. Display employees whose salary is less than or equal to 60,000.
+
+
 USE SalesDB
 SELECT * 
 FROM Sales.Employees
@@ -13,4 +20,5 @@ USE SalesDB
 SELECT * 
 FROM Sales.Employees
 WHERE Salary>=60000
+
 
