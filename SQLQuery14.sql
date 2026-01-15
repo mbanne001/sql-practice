87ea6ed4ee8cb94a11fd15c56e18ec01cedc348b
mbanne001 <mbanne001@gmail.com>
2026-01-14 20:54:29 -0600

    Update SQLQuery14.sql

--- SQLQuery14.sql
+++ SQLQuery14.sql
@@ -1,3 +1,11 @@
+--Update the salary of an employee whose EmployeeID is 105.
+--Increase the salary of all employees in the IT department by 10%.
+--Update multiple columns (Department and Salary) in a single UPDATE query.
+--Update product prices by adding $5 where price is less than $50.
+--Write a query to change the country of a customer from ‘USA’ to ‘United States’.
+
+
+
 USE SalesDB
 UPDATE  Sales.Employees
 SET salary=60000
@@ -24,4 +32,5 @@ WHERE country='USA'
 
 USE MyDatabase
 SELECT *
+
 FROM customers
