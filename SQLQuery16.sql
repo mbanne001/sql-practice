fb9cffb5805780f2aab8e44c5861f248c98f4d68
mbanne001 <mbanne001@gmail.com>
2026-01-15 06:28:47 -0600

    Add files via upload

---
+++ SQLQuery16.sql
@@ -0,0 +1,16 @@
+USE SalesDB
+SELECT * 
+FROM Sales.Employees
+WHERE Salary=50000
+
+
+USE SalesDB
+SELECT *
+FROM Sales.Products
+	WHERE	Price>200
+
+USE SalesDB
+SELECT * 
+FROM Sales.Employees
+WHERE Salary>=60000
+
