565040dc4358a1301a01273a6792e3abd79fed98
mbanne001 <mbanne001@gmail.com>
2026-01-16 07:42:52 -0600

    Update SQLQuery18.sql

--- SQLQuery18.sql
+++ SQLQuery18.sql
@@ -1,3 +1,9 @@
+-- Find products with price between 100 and 300.
+
+-- Display employees whose salary is between 50,000 and 80,000.
+
+-- Show orders where sales amount is between 200 and 1,000.
+
 USE SalesDB
 SELECT *
 FROM Sales.Products
@@ -13,4 +19,5 @@ WHERE Salary BETWEEN 50000 AND 80000
 USE SalesDB
 SELECT *
 FROM Sales.Orders
+
 WHERE Sales BETWEEN 200 AND 1000
