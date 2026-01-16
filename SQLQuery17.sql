6c89d0c2ebd3d75a07a540f2d66894f718596986
mbanne001 <mbanne001@gmail.com>
2026-01-16 07:42:01 -0600

    Update SQLQuery17.sql

--- SQLQuery17.sql
+++ SQLQuery17.sql
@@ -1,3 +1,9 @@
+-- Find employees who work in IT AND earn more than 70,000.
+
+-- Show customers who are from USA OR India.
+
+-- Display orders that are NOT having sales less than 100.
+
 USE SalesDB
 SELECT * 
 FROM Sales.Employees
@@ -11,4 +17,5 @@ WHERE country IN ('USA','INDIA')
 USE SalesDB
 SELECT *
 FROM Sales.Orders
+
 WHERE NOT Sales<100
