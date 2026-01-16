de100ad3319d3011c25052d5b3a29ca847f8349c
mbanne001 <mbanne001@gmail.com>
2026-01-16 07:43:34 -0600

    Update SQLQuery19.sql

--- SQLQuery19.sql
+++ SQLQuery19.sql
@@ -1,3 +1,10 @@
+-- Find employees working in IT, HR, or Finance departments.
+
+-- Display orders placed by customers with IDs 1, 3, and 5.
+
+-- Show products belonging to categories Electronics or Clothing.
+
+
 USE SalesDB
 SELECT * 
 FROM Sales.Employees
@@ -12,4 +19,5 @@ WHERE CustomerID IN (1,3,5)
 USE SalesDB
 SELECT *
 FROM Sales.Products
+
 WHERE Category IN ('Electronics','Clothing')
